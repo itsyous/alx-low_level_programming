2a8616450d5174413911a6960ace004ed94fc239
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-20 04:47:46 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * writing a program that prints putchar
+ * Return - Always 0 (Success).
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');	
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');	
+	Return (0);
+}
+
