fa3c4dbdd4359066cdb34767dd0a21e992c994b2
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-20 13:44:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
- * Print alphabet - print the alphabet in lowercase
- *
- * Return: void
+ * print_alphabet - Entry point
+ * Return:0
  */
+
 void print_alphabet(void)
 {
 	char c = 'a';
