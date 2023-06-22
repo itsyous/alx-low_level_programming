0d1ab4576e36ade61a39af6618d496dfa83d495e
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-22 09:57:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return:0
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48: n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar("\n");
+	return (0);
+}
