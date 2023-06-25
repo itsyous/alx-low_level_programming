01c8bc4a1af9053d18ffa00b00c9e6fb88443027
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-25 12:37:08 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@ void print_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
-	{
-		_putchar(x);
-		_putchar("\n");
-	}
+	for {
+		_putchar(x + 48);
+		x++;
+	} while (x >= 0 && x <= 9);
+	_putchar('\n');
 }
