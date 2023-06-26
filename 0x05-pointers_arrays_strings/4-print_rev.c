ee5628d387dae2e98d6df46007bcadf1752745f9
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-26 10:35:28 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,15 +13,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
-	{
+	while (s[i])
 		i++;
-	}
-	while (i >= 0)
+
+	while (i--)
 	{
 		_putchar(s[i]);
-		i--;
 	}
 
 	_putchar('\n');
+
 }
