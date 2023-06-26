885e5606576c2e3c7d322d8c411bfa7812862c86
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-26 05:31:09 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,12 +5,11 @@
  * Return: void
  */
 
-
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i]);
+	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
@@ -18,4 +17,3 @@ void _puts(char *str)
 
 	_putchar('\n');
 }
-
