8e22aab3afc097bd2cc4058fcd65a5f7a8fe32dd
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-26 13:17:57 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,20 @@
 int _putchar(char c);
 
 /**
- * print numbers - print the numbers since 0 up to 9
- * Return: The numbers since 0 up to 9
+ * print_numbers - prints numbers
+ * Return:0
  */
+
 void print_numbers(void)
 {
-	int x;
+	char n = 0;
 
-	for {
-		_putchar(x + 48);
-		x++;
-	} while (x >= 0 && x <= 9);
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+
+	}
 	_putchar('\n');
+
 }
