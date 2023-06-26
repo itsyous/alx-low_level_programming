01924fd2b87f0112438836e5d4941bbf7de30864
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-26 09:28:09 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _puts - prints reversed string, followed by a new line
+ * _puts - prints reversed string, followed by a new line,
  * @c: the_character_to_be_written
  * Return: On Success
 */
 int _putchar(char c);
 /**
- * _puts - prints reversed string, followed by a new line
+ * _puts - prints reversed string, followed by a new line,
  * @str: pointer_to_be
  */
 void print_rev(char *s)
