28efd3a400942b4ae72beff0f8f10bf18db679e6
YoussefRabih <youssef.rabih1995@gmail.com>
2023-06-26 11:08:30 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,10 +3,9 @@
 int _putchar(char c);
 
 /**
- * puts2 - prints a string, followed by a new line,
+ * print_rev - prints a strings, followed by a new line,
  * @s: the string_to_be_printed
  */
-
 void puts2(char *str)
 {
 	int n = 0;
