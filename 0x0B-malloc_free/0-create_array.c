4a9d937baab65df1eebca6126b17b557d72ddaf8
itsyous <youssef.rabih1995@gmail.com>
2023-07-29 05:23:22 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,21 +9,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *s;
-	unsigned int i;
+	char *n = malloc(size);
 
-	if (size == 0)
-		return (NULL);
+	if (size == 0 || n == 0)
+		return (0);
 
-	s = malloc(size * sizeof(char));
+	while (size--)
+		n[size] = c;
 
-	if (s == NULL)
-		return (NULL);
 
-	for (i = 0; i < size; i++)
-	{
-		s[i] = c;
-	}
-
-	return (s);
+	return (n);
 }
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * print_tab - prints an array of string
+ * @tab: the array to print
+ * Return: Nothing
+ */
+char **strtow(char *str)
+{
+	int i;
+
