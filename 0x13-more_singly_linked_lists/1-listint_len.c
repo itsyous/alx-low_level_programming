d9fe4d23a69ad5a5df8cb9fbd6f697e0fa538ca4
itsyous <youssef.rabih1995@gmail.com>
2023-08-01 14:33:32 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 
 /**
- * listint_len - prints a linked list
+ * print_listint - prints a linked lists
  * @h: pointer to first node
  * Return: size of list
  */
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	const listint_t *temp;
+	unsigned int i = 0;
 
-	while (h)
+	temp = h;
+	while (temp != NULL)
 	{
-		h = h->next;
+		temp = temp->next;
 		i++;
 	}
 	return (i);
