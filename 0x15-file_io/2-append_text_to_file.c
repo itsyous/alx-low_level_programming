bfc7cc0e712a0dee8f3c00c6be6e83a482b5190f
itsyous <youssef.rabih1995@gmail.com>
2023-11-27 13:34:47 -0800

    append txt to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,12 +18,12 @@ int _strlen(char *s)
 }
 
 /**
- * create_file - creates a file
+ * append_text_to_file - append a text to a file
  * @filename: name of file to create
  * @text_content: text to write
  * Return: 1 on success
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
