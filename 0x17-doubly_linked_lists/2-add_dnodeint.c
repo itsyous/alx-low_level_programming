74cbb66f5de0ff056a48c99804080575c2aef964
itsyous <youssef.rabih1995@gmail.com>
2024-01-11 15:20:42 -0800

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,9 +18,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
-	else if (*head == NULL)
-		new_node->next = NULL;
-
 	*head = new_node;
 	if (new_node->next != NULL)
 		(new_node->next)->prev = new_node;
