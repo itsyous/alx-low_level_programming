d11253eb48cac11d2571f3f1d5b158808efc981e
itsyous <youssef.rabih1995@gmail.com>
2024-01-11 15:52:14 -0800

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t i;
 
-	while (h != NULL)
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
 	}
 	return (i);
 }
