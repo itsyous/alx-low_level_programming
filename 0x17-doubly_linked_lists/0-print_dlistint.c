9d4f64a7bf3900788ec174850329113ad029affb
itsyous <youssef.rabih1995@gmail.com>
2024-01-11 16:04:58 -0800

    print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,14 +2,14 @@
 
 /**
  * print dlistint_t - prints doubly linked list
- * @h: address of head node
+ * @h: pointer to first node
  * Return: size of list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
-	for (i = 0; h != NULL; i++)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
