b0a6551c65c4c451baaf9c07e94b8d04fe04df14
itsyous <youssef.rabih1995@gmail.com>
2024-02-21 05:37:20 -0800

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,9 +11,9 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
-	insigned long int index;
+	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
