ef5ab7622eacb5db48413d0cf274fd65ed288374
itsyous <youssef.rabih1995@gmail.com>
2024-03-11 17:04:42 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	ptr = malloc(sizeof(char) * size);
 
-	if (size == 0)
+	if (size == 0 || ptr == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
